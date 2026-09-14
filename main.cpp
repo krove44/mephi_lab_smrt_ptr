f998df6b20a69bac2b0af9c34e99dbb9421e2d1e
Konstantin Ponomarev <krove8@gmail.com>
2026-09-15 00:18:47 +0300

    add operator= and constructor copy

--- main.cpp
+++ main.cpp
@@ -2,11 +2,8 @@
 #include "smart_ptr.h"
 
 int main() {
-    int* p = new int(10);
-    smart_ptr ptr(p);
-
-    std::cout << ptr << std::endl;
-
-    delete p;
+    smart_ptr<int> ptr(new int(10));
+    smart_ptr<int> ptr2 = ptr;
+    std::cout << *ptr2 << std::endl;
     return 0;
 }
--- smart_ptr.h
+++ smart_ptr.h
@@ -5,20 +5,53 @@ template <typename T>
 class smart_ptr {
 private:
     T* ptr;
-
+    size_t* counter;
 public:
-    explicit smart_ptr(T* ptr) : ptr(ptr) {}
+    explicit smart_ptr() : ptr(nullptr), counter(nullptr) {}
+
+    explicit smart_ptr(T* ptr) : ptr(ptr), counter(new size_t(1)) {}
+
+    smart_ptr(smart_ptr<T>& other) : ptr(other.ptr), counter(other.counter) {
+        if (counter) {
+            ++*counter;
+        }
+    }
+
     ~smart_ptr() {
-        delete ptr;
+        if (counter && --*counter == 0) {
+            delete ptr;
+            delete counter;
+        }
+        ptr = nullptr;
+        counter = nullptr;
+    }
+
+    const smart_ptr<T>&  operator=(smart_ptr<T>& other) {
+        if (this != &other) {
+            this->~smart_ptr();
+            this->ptr = other.ptr;
+            this->counter = other.counter;
+            if (counter) ++*counter;
+
+        }
+        return *this;
     }
 
     T& operator*() {
         return *ptr;
     }
 
+    const T& operator*() const {
+        return *ptr;
+    }
+
+    T* operator->() {
+        return ptr;
+    }
+
     friend std::ostream& operator<<(std::ostream& os, const smart_ptr<T>& sp) {
         os << sp.ptr;
         return os;
     }
-
+    
 };
