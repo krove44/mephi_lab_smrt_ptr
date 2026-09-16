ac5d8de895b48f4c7fc7d8a70aff3c52cd5103e7
Konstantin Ponomarev <krove8@gmail.com>
2026-09-16 23:07:52 +0300

    add uniq_ptr for T[]

--- ptr/uniq_ptr.h
+++ ptr/uniq_ptr.h
@@ -34,3 +34,36 @@ public:
     T* get() const {return ptr_;}
 
 };
+
+
+template <typename T>
+class uniq_ptr<T[]> {
+private:
+    T* ptr_;
+public:
+    explicit uniq_ptr(T* ptr = nullptr) : ptr_(ptr) {}
+
+    uniq_ptr(const uniq_ptr&) = delete;
+    uniq_ptr& operator=(const uniq_ptr&) = delete;
+
+    uniq_ptr(uniq_ptr&& other) noexcept : ptr_(other.ptr_) {
+        other.ptr_ = nullptr;
+    }
+
+    ~uniq_ptr() {
+        delete[] ptr_;
+        ptr_ = nullptr;
+    }
+
+    uniq_ptr& operator=(uniq_ptr&& other) noexcept {
+        if (this != &other) {
+            delete[] ptr_;
+            ptr_ = other.ptr_;
+            other.ptr_ = nullptr;
+        }
+        return *this;
+    }
+
+    T& operator[](size_t i) const { return ptr_[i]; }
+    T* get() const { return ptr_; }
+};
