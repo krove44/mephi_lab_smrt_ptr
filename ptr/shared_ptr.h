cf2d8d9e8df1292317727bf8ea1001a3532c21e1
Konstantin Ponomarev <krove8@gmail.com>
2026-09-16 23:24:16 +0300

    add shared_ptr.h for T[]

--- ptr/shared_ptr.h
+++ ptr/shared_ptr.h
@@ -65,3 +65,51 @@ public:
     }
 
 };
+
+
+template <typename T>
+class shared_ptr<T[]> {
+private:
+    T* ptr;
+    size_t* counter;
+    void clear() {
+        if (counter && --*counter == 0) {
+            delete[] ptr;
+            delete counter;
+        }
+    }
+public:
+    explicit shared_ptr() : ptr(nullptr), counter(nullptr) {}
+    shared_ptr(T* ptr) : ptr(ptr), counter(new size_t(1)) {}
+    shared_ptr(const shared_ptr<T[]>& other) : ptr(other.ptr), counter(other.counter) {
+        if (counter) ++*counter;
+    }
+
+    ~shared_ptr() {
+        clear();
+        ptr = nullptr;
+        counter = nullptr;
+    }
+
+    shared_ptr<T[]>& operator=(const shared_ptr<T[]>& other) {
+        if (this != &other) {
+            clear();
+            this->ptr = other.ptr;
+            this->counter = other.counter;
+            if (counter) ++*counter;
+        }
+        return *this;
+    }
+
+    T& operator[](size_t i) { return ptr[i]; }
+    const T& operator[](size_t i) const { return ptr[i]; }
+
+    T* get() const { return ptr; }
+    size_t* get_counter() const { return counter; }
+    size_t use_count() const { return counter ? *counter : 0; }
+
+    friend std::ostream& operator<<(std::ostream& os, const shared_ptr<T[]>& sp) {
+        os << sp.ptr;
+        return os;
+    }
+};
